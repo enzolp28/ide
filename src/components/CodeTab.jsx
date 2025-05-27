51b9cccb8bdab4384fca11961bdf0dacdc359e27
votre_nom <laposta.enzo@gmail.com>
2025-05-27 10:03:33 +0200

    sidebar & textarea

---
+++ src/components/CodeTab.jsx
@@ -0,0 +1,9 @@
+
+
+export default function CodeTab() {
+    return (
+        <textarea spellCheck="false" className="bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none">
+
+        </textarea>
+    )
+}
